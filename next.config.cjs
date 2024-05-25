1d3adfe65ce1ca405315064c50d001edc3669c4c
Douglas <douglas.aguiar410@gmail.com>
2024-05-25 16:27:00 -0300

    feat: fazendo configurações para transformar aplicaçção em pwa

---
+++ next.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})
+
+export default withPWA(nextConfig)
