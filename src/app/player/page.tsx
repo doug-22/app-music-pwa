1060b4cb767d62b67ee957def121ef81647ada35
Douglas <douglas.aguiar410@gmail.com>
2024-06-18 10:34:51 -0300

    feat: finalizando tela do player

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -7,30 +7,56 @@ import {
   WrapperPreview,
   WrapperList,
   Divider,
+  WrapperListPlaylist,
 } from './styles'
 import InputSearch from '@/components/InputSearch'
 import Button from '@/components/Button'
 import { DataType } from '@/components/SongDetails'
 
 import Player from '@/components/Player'
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import ListSongs from '@/components/ListSongs'
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
 import { listMusicAtom } from '@/atoms/list-music-atom'
 import { musicSelectedAtom } from '@/atoms/music-selected-atom'
-import { playlistSelectedAtom } from '@/atoms/playlist-selected-atom'
+import {
+  playlistModeAtom,
+  playlistSelectedAtom,
+} from '@/atoms/playlist-selected-atom'
+import { recentsMusicAtom } from '@/atoms/recents-music-atom'
+import { favoritesMusicAtom } from '@/atoms/favorites-music-atom'
+import { toast } from 'react-toastify'
+import { playlistsAtom } from '@/atoms/playlists-atom'
 
 const PlayerPage = () => {
   const [search, setSearch] = useState('')
 
   const setMusicSelected = useSetAtom(musicSelectedAtom)
+  const [recentsMusics, setRecentsMusics] = useAtom(recentsMusicAtom)
+  const [favoritesMusic, setFavoritesMusic] = useAtom(favoritesMusicAtom)
+  const playlist = useAtomValue(playlistsAtom)
+  const [playlistMode, setPlaylistMode] = useAtom(playlistModeAtom)
   const list = useAtomValue(listMusicAtom)
 
   const [playlistSelected, setPlaylistSelected] = useAtom(playlistSelectedAtom)
 
-  const handleSelectedMusic = (data: DataType) => {
-    setMusicSelected(data)
-  }
+  const handleSelectedMusic = useCallback(
+    (data: DataType) => {
+      setMusicSelected(data)
+
+      const index = recentsMusics?.findIndex(
+        (item) => item?.title === data?.title,
+      )
+
+      if (index === -1) {
+        setRecentsMusics([data, ...recentsMusics])
+      } else {
+        const newArray = recentsMusics.toSpliced(index, 1)
+        setRecentsMusics([data, ...newArray])
+      }
+    },
+    [recentsMusics],
+  )
 
   useEffect(() => {
     setPlaylistSelected(list)
@@ -40,12 +66,79 @@ const PlayerPage = () => {
     return (
       playlistSelected?.filter((item) => {
         return (
-          item?.title.toLocaleLowerCase().includes(search) ||
-          item?.singer.toLocaleLowerCase().includes(search)
+          item?.title
+            .toLocaleLowerCase()
+            .includes(search.toLocaleLowerCase()) ||
+          item?.singer.toLocaleLowerCase().includes(search.toLocaleLowerCase())
         )
       }) ?? []
     )
   }, [playlistSelected, search])
+
+  const handleSetShuffleMusic = useCallback(() => {
+    const playlistRandom = playlistFiltered?.sort(() => Math.random() - 0.5)
+    setPlaylistSelected(playlistRandom)
+    setMusicSelected(playlistRandom[0])
+  }, [playlistFiltered, setPlaylistSelected, setMusicSelected])
+
+  const handleSetPlaylistRecentsMode = useCallback(() => {
+    if (playlistMode === 'recents') {
+      setPlaylistMode('default')
+      return
+    }
+    setPlaylistMode('recents')
+  }, [playlistMode])
+
+  const handleSetFavoritesMode = useCallback(() => {
+    if (playlistMode === 'favorites') {
+      setPlaylistMode('default')
+      return
+    }
+    setPlaylistMode('favorites')
+  }, [playlistMode])
+
+  const handleSetPlaylistsMode = useCallback(() => {
+    if (['playlist-list', 'playlist-songs'].includes(playlistMode)) {
+      setPlaylistMode('default')
+      return
+    }
+    setPlaylistMode('playlist-list')
+  }, [playlistMode])
+
+  const handleFavoriteMusic = useCallback(
+    (data: DataType) => {
+      const index = favoritesMusic?.findIndex((item) => {
+        return item?.title === data?.title
+      })
+      if (index === -1) {
+        setFavoritesMusic([...favoritesMusic, data])
+        toast.success('Music add to favorites!')
+      } else {
+        const newArray = favoritesMusic?.toSpliced(index, 1)
+        setFavoritesMusic([...newArray])
+        toast.success('Music removed from favorites!')
+      }
+    },
+    [favoritesMusic],
+  )
+
+  const handleSelectPlaylist = useCallback(
+    (songs: DataType[]) => {
+      setPlaylistSelected(songs)
+      setPlaylistMode('playlist-songs')
+    },
+    [playlist],
+  )
+
+  useEffect(() => {
+    if (playlistMode === 'favorites') {
+      setPlaylistSelected(favoritesMusic)
+    } else if (playlistMode === 'recents') {
+      setPlaylistSelected(recentsMusics)
+    } else if (playlistMode === 'default') {
+      setPlaylistSelected(list)
+    }
+  }, [playlistMode, favoritesMusic, recentsMusics, list])
 
   return (
     <>
@@ -53,39 +146,76 @@ const PlayerPage = () => {
         <InputSearch width="100%" onChange={(e) => setSearch(e.target.value)} />
         <WrapperOptions>
           <Button
-            icon={<Clock size={32} weight="fill" />}
+            icon={
+              <Clock
+                size={32}
+                weight={playlistMode === 'recents' ? 'fill' : 'regular'}
+              />
+            }
             align="vertical"
-            label="Recent"
+            label="Recents"
+            onClick={handleSetPlaylistRecentsMode}
           />
           <Button
-            icon={<Heart size={32} weight="fill" />}
+            icon={
+              <Heart
+                size={32}
+                weight={playlistMode === 'favorites' ? 'fill' : 'regular'}
+              />
+            }
             align="vertical"
             label="Favorites"
+            onClick={handleSetFavoritesMode}
           />
           <Button
-            icon={<Playlist size={32} weight="fill" />}
+            icon={
+              <Playlist
+                size={32}
+                weight={
+                  ['playlist-list', 'playlist-songs'].includes(playlistMode)
+                    ? 'fill'
+                    : 'regular'
+                }
+              />
+            }
             align="vertical"
             label="Playlist"
+            onClick={handleSetPlaylistsMode}
           />
         </WrapperOptions>
         <WrapperPreview>
           <Button
             icon={<Shuffle size={32} weight="fill" />}
-            label="Ordem aleatória"
+            label="Random order"
+            onClick={handleSetShuffleMusic}
           />
         </WrapperPreview>
         <Divider>
           <span>List</span>
         </Divider>
         <WrapperList>
-          <ListSongs
-            list={playlistFiltered}
-            setSelectedMusic={handleSelectedMusic}
-          />
+          {playlistMode === 'playlist-list' ? (
+            <WrapperListPlaylist>
+              {playlist?.map((item, idx) => (
+                <li key={idx} onClick={() => handleSelectPlaylist(item?.songs)}>
+                  <h3>{item?.title}</h3>
+                  <span>
+                    <strong>Total songs:</strong> {item?.songs.length}
+                  </span>
+                </li>
+              ))}
+            </WrapperListPlaylist>
+          ) : (
+            <ListSongs
+              list={playlistFiltered}
+              setSelectedMusic={handleSelectedMusic}
+              setFavoriteMusic={handleFavoriteMusic}
+            />
+          )}
         </WrapperList>
       </WrapperContainer>
 
-      <Player />
+      <Player setFavoriteMusic={handleFavoriteMusic} />
     </>
   )
 }
